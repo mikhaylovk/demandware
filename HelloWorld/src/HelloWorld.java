0bb8a8b35c04ba7f29cb37054e35422f267ba80c
kmikhaylov <kmikhaylov@ct-aschaferlp.nyc.createthegroup.com>
2014-04-11 16:40:42 -0400

    Initial committ

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * This is my first Eclipse project... Hello World
+ * @author kmikhaylov
+ *@version 1.0
+ */
+public class HelloWorld {
+
+	/**
+	 * The method that actually does the saying of HEllo World.
+	 * @param args Don't worry about this parameter
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello world");
+
+	}
+
+}
