962b497e70c8204c07c12dc367be063836cb3e9b
mikhaylovk <constantinem@gmail.com>
2014-04-16 14:38:32 -0400

    New feature 3

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld {
 	/**
 	 * The method that actually does the saying of HEllo World.
 	 * @param args Don't worry about this parameter
+	 * Feature 3
 	 */
 	public static void main(String[] args) {
 		System.out.println("hello world");
